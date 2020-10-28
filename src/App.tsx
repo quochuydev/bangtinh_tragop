6603fa07d0041552e3f3f03bb157efb210d13a4c
quochuydev <quochuy.dev@gmail.com>
2020-10-28 20:37:55 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import Box from '@material-ui/core/Box';
+import Link from '@material-ui/core/Link';
 
-function App() {
+export default function App() {
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setAge(event.target.value as string);
+  };
+
   return (
-    <div>
-      Edit <code>src/App.tsx</code> and save to reload.
-    </div>
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Create React App v4-beta example with TypeScript
+        </Typography>
+        <Copyright />
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={age}
+          onChange={handleChange}
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </Box>
+    </Container>
   );
 }
 
-export default App;
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="#">
+        quochuydev
+      </Link>{' '}
+      {new Date().getFullYear()}.
+    </Typography>
+  );
+}
