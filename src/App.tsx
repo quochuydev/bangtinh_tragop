8ffe923d52af0b15d2ef35aefafc0cf0dcedc6be
quochuydev <quochuy.dev@gmail.com>
2020-10-28 21:45:11 +0700

    x

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
 import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import {
   Input, Table, TableContainer, TableCell, TableRow, TableHead, TableBody,
-  MenuItem, Box, Link, Select, Typography, Container, Paper,
+  MenuItem, Box, Link, Select, Typography, Container, Paper, TextField,
+  NativeSelect, FormControl, InputLabel,
 } from '@material-ui/core';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme: Theme) => createStyles({
   table: {
     minWidth: 650,
   },
-});
+  formControl: {
+    // margin: theme.spacing(1),
+    minWidth: 120,
+  },
+}));
 
 export default function App() {
   const [age, setAge] = useState('');
+  const initData = {
+    carPrice: 0,
+  }
+  const [data, setData] = useState(initData);
   const [carPrice, setCarPrice] = useState(0);
   const [borrowingPrice, setBorrowingPrice] = useState(0);
   const [borrowingMonthTotal, setBorrowingMonthTotal] = useState(0);
@@ -25,19 +34,21 @@ export default function App() {
   ]
 
 
-  function createData(name: string, calories: number, fat: number, carbs: number, protein: number) {
-    return { name, calories, fat, carbs, protein };
+  function createData(period: number, recurring_number: number, interest: number, origin: number, debt: number) {
+    return { period, recurring_number, interest, origin, debt };
   }
 
   let rows = [
-    createData('', 0, 0, 0, 0),
+    createData(1, 0, 0, 0, 0),
   ]
   function refreshCalculate() {
 
   }
 
-  function onChangeField() {
-
+  let onChangeField = (e: React.ChangeEvent<{ name: string, value: unknown }>) => {
+    console.log(e.target.name)
+    console.log(e.target.value)
+    setData({ ...data, [e.target.name]: e.target.value })
   }
 
   const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
@@ -55,39 +66,44 @@ export default function App() {
         <Typography variant="h5" component="h5" gutterBottom>
           SẢN PHẨM TRUYỀN THỐNG - THANH TOÁN GỐC LÃI  HÀNG THÁNG
         </Typography>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={age}
-          onChange={handleChange}
-        >
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-        <Input />
+
+        <FormControl className={classes.formControl}>
+          <InputLabel shrink htmlFor="age-native-label-placeholder">
+            Age
+        </InputLabel>
+          <NativeSelect
+            value={10}
+            onChange={handleChange}
+            inputProps={{ name: 'age', id: 'age-native-label-placeholder', }}          >
+            <option value="">None</option>
+            <option value={10}>Ten</option>
+            <option value={20}>Twenty</option>
+            <option value={30}>Thirty</option>
+          </NativeSelect>
+        </FormControl>
+
+        <TextField id="carPrice" label="Giá xe" defaultValue={initData.carPrice}
+          type="number" name="carPrice" onChange={onChangeField} />
 
         <TableContainer component={Paper}>
           <Table className={classes.table} aria-label="simple table">
             <TableHead>
               <TableRow>
-                <TableCell>Dessert (100g serving)</TableCell>
-                <TableCell align="right">Calories</TableCell>
-                <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                <TableCell align="right">Kỳ</TableCell>
+                <TableCell align="right">Số trả định kỳ</TableCell>
+                <TableCell align="right">Lãi</TableCell>
+                <TableCell align="right">Gốc</TableCell>
+                <TableCell align="right">Dư nợ</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
               {rows.map((row) => (
-                <TableRow key={row.name}>
-                  <TableCell component="th" scope="row">
-                    {row.name}
-                  </TableCell>
-                  <TableCell align="right">{row.calories}</TableCell>
-                  <TableCell align="right">{row.fat}</TableCell>
-                  <TableCell align="right">{row.carbs}</TableCell>
-                  <TableCell align="right">{row.protein}</TableCell>
+                <TableRow key={row.period}>
+                  <TableCell align="right">{row.period}</TableCell>
+                  <TableCell align="right">{row.recurring_number}</TableCell>
+                  <TableCell align="right">{row.interest}</TableCell>
+                  <TableCell align="right">{row.origin}</TableCell>
+                  <TableCell align="right">{row.debt}</TableCell>
                 </TableRow>
               ))}
             </TableBody>
