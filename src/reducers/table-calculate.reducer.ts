a2796157ee045fafaae0f690e9a8f16a2952e40e
quochuydev <quochuy.dev@gmail.com>
2020-10-29 21:48:00 +0700

    update

--- src/reducers/table-calculate.reducer.ts
+++ src/reducers/table-calculate.reducer.ts
@@ -1,4 +1,5 @@
 import { BaseAction, TableCalculate, PeriodLine } from '../common'
+import * as _ from 'lodash';
 
 export type TableCalculateState = TableCalculate;
 
@@ -12,7 +13,6 @@ export const tableCalculateReducer = (
     precentBankBefore: 0.699,
     precentBankAfter: 0.1085,
     discountMonths: 6
-
   },
   action: BaseAction
 ) => {
@@ -27,7 +27,7 @@ export const tableCalculateReducer = (
         if (action.payload['prepayPercent']) {
           data.borrowingPrice = (data.prepayPercent * data.carPrice) / 100;
         }
-        if (action.payload['borrowingPrice'] || action.payload['carPrice']) {
+        if (_.isEmpty(action.payload) || action.payload['borrowingPrice'] || action.payload['carPrice']) {
           data.prepayPercent = (data.borrowingPrice / data.carPrice) * 100;
         }
       }
@@ -36,7 +36,7 @@ export const tableCalculateReducer = (
       data.rows = [createData(0, 0, 0, 0, data.borrowingPrice)];
       for (let i = 1; i <= data.month; i++) {
         let data_row = {
-          precentBank: 0, debt_last: 0, interest: 0, recurring_number: 0,
+          precentBank: 0, debt_last: 0, interest: 0, recurring_number: 0, origin: 0, debt: 0,
         };
         if (i <= data.discountMonths) {
           data_row.precentBank = 0.0699;
@@ -44,9 +44,18 @@ export const tableCalculateReducer = (
           data_row.precentBank = 0.1085;
         }
         data_row.debt_last = data.rows[i - 1]['debt'];
-        data_row.interest = data_row.debt_last * data_row.precentBank / 12
-        data_row.recurring_number = data_row.interest + pricePerMonth;
-        let row = createData(i, data_row.recurring_number, data_row.interest, pricePerMonth, data_row.debt_last - pricePerMonth);
+        data_row.interest = data_row.debt_last * data_row.precentBank / 12;
+
+        data_row.origin = pricePerMonth;
+        if (i == data.month) {
+          data_row.origin = data_row.debt_last;
+        }
+
+        data_row.recurring_number = data_row.interest + data_row.origin;
+        data_row.debt = data_row.debt_last - data_row.origin;
+        data_row.debt = roundNumber(data_row.debt);
+
+        let row = createData(i, roundNumber(data_row.recurring_number), roundNumber(data_row.interest), roundNumber(data_row.origin), data_row.debt);
         data.rows.push(row);
       }
       return data;
@@ -54,6 +63,10 @@ export const tableCalculateReducer = (
       return state;
   }
 };
+
+function roundNumber(input: number) {
+  return Math.round(input * 1) / 1;
+}
 
 function createData(period: number, recurring_number: number, interest: number, origin: number, debt: number) {
   return { period, recurring_number, interest, origin, debt };
